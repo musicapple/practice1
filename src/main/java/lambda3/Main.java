bac944da5d61dd3309fbef1adb63f0167d197ce7
musicapple <jaker0109@naver.com>
2024-10-03 20:49:56 +0900

    람다 연습문제 커밋

---
+++ src/main/java/lambda3/Main.java
@@ -0,0 +1,15 @@
+package lambda3;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        StringLengthCalculator calculator = str -> str.length();
+        System.out.println(calculator.getLength("Lambda Expressions"));
+    }
+}
+
+@FunctionalInterface
+interface StringLengthCalculator {
+    int getLength(String str);
+}
