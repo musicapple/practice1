8da025e774e25efca53fc92c96786a4bb1001c47
musicapple <jaker0109@naver.com>
2024-10-03 20:26:26 +0900

    람다 연습문제 커밋

---
+++ src/main/java/lambda1/Main.java
@@ -0,0 +1,18 @@
+package lambda1;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        // 1번 방법
+        ABC number = (a) -> a>=100;
+
+        System.out.println(number.abc(55));
+    }
+
+}
+
+@FunctionalInterface
+interface ABC{
+    boolean abc(int a);
+}
