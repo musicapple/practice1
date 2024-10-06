d8d159960b7a02a5fce5f80cc51810f0295afd49
musicapple <jaker0109@naver.com>
2024-10-06 23:39:08 +0900

    User 및 UserDataManager 작성 로그인 기능

--- src/main/java/sign_up/Main.java
+++ src/main/java/sign_up/Main.java
@@ -1,6 +1,8 @@
 package sign_up;
 
- /*
+import java.util.Scanner;
+
+/*
  유저의 데이터를 저장하는 UserDataManager 클래스 작성
  UserDataManager 는 User 객체의 register를 도와줌 (가입)
  UserDataManager 는 User 객체가 이미 회원인지 확인 가능 (id를 통해)
@@ -9,8 +11,50 @@ package sign_up;
  */
  public class Main {
 
-     public static void main(String[] args) {
+    public static void main(String[] args) {
 
+/*
+        방법 1
 
-     }
+        UserDataManager manager = UserDataManager.getInstance();
+
+        Scanner sc = new Scanner(System.in);
+        while (true) {
+            String id = sc.next();
+            String pw = sc.next();
+
+            if (!manager.isMember(id)) {
+                manager.register(id, pw);
+                System.out.println("멤버가 아니여서 등록하였습니다.");
+            } else if (manager.isMember(id) && manager.getUser(id).getPw().equals(pw)) {
+                System.out.println("로그인 성공!");
+                break;
+            } else if (manager.isMember(id) && !manager.getUser(id).getPw().equals(id)) {
+                System.out.println("비밀번호가 다릅니다.");
+                break;
+            }
+        }*/
+
+        // 방법 2
+        UserDataManager manager = UserDataManager.getInstance();
+        Scanner sc = new Scanner(System.in);
+        while (true) {
+            System.out.print("ID: ");
+            String id = sc.next();
+            System.out.print("PW: ");
+            String pw = sc.next();
+
+            if (!manager.isMember(id)) {
+                manager.register(id, pw);
+                System.out.println("멤버가 아니여서 등록하였습니다.");
+            } else {
+                User user = manager.getUser(id);
+                if (user.getPw().equals(pw)) {
+                    System.out.println("로그인 되었습니다!");
+                } else {
+                    System.out.println("비밀번호가 다릅니다.");
+                }
+            }
+        }
+    }
 }
--- src/main/java/sign_up/UserDataManager.java
+++ src/main/java/sign_up/UserDataManager.java
@@ -8,7 +8,7 @@ public class UserDataManager {
     // 만약 여러 인스턴스가 생성된다면 서로 다른 객체가 서로 다른 유저 데이터를 관리할 수 있게 되고,
     // 데이터가 일관되지 않을 수 있음
     private static UserDataManager instance;
-    private static UserDataManager getInstance(){
+    public static UserDataManager getInstance(){
         if(instance == null){
             instance = new UserDataManager();
         }
@@ -16,21 +16,27 @@ public class UserDataManager {
     }
     private UserDataManager(){}
 
-
+    Map<String, User> map = new HashMap<>();
 
     public void register(String id, String pw){
-
+        map.put(id,new User(id,pw));
     }
 
     public boolean isMember(String id){
-
+        return map.containsKey(id);
     }
 
     public User getUser(String id){
-
+        return map.get(id);
     }
 
     public boolean unregister(User user){
-
+        if(isMember(user.getId())){
+            map.remove(user.getId());
+            return true;
+        } else{
+            System.out.println("회원 정보가 없습니다");
+            return false;
+        }
     }
 }
